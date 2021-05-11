8139e9668298bed3a3567e99f69a36ddeebcdc82
joroovb <jorisvanbreugel@gmail.com>
2021-05-11 21:32:52 +0200

    fixed double looping

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+
 	db := dobbelsteen.New()
 	sp := Speler.New("Jaap")
 	so := Speler.New("Jan")
@@ -17,7 +17,8 @@ func main() {
 	klaar := false
 	for !klaar {
 	game:
-		for i, _ := range spelers {
+		for i := range spelers {
+			reader := bufio.NewReader(os.Stdin)
 			fmt.Printf("%v is aan de beurt. q om te stoppen, d om te dobbelen\n", spelers[i].Naam())
 			char, _, err := reader.ReadRune()
 
@@ -25,29 +26,27 @@ func main() {
 				fmt.Println(err)
 			}
 
-			switch char {
-			case 'q':
+			if char == 'q' {
 				os.Exit(0)
-			case 'd':
-				spelers[i].SetPositie(db.Rollen())
-				fmt.Printf("%v staat op %v\n\n", spelers[i].Naam(), spelers[i].Positie())
-				switch spelers[i].Positie() {
-				case 63:
-					fmt.Printf("%v heeft gewonnen!\n", spelers[i].Naam())
-					klaar = true
-					break game
-				case 10, 20, 30, 40, 50, 60:
-					fmt.Println("Dubbele stappen!")
-					spelers[i].SetPositie(db.LaatsteWorp())
-					fmt.Printf("Je staat nu op %v\n", spelers[i].Positie())
-				case 23:
-					fmt.Println("Je gaat naar de gevangenis!\nGame over!")
-					klaar = true
-					break game
-				case 25, 45:
-					fmt.Println("Terug naar start!")
-					sp.ToStart()
-				}
+			}
+			spelers[i].SetPositie(db.Rollen())
+			fmt.Printf("%v staat op %v\n\n", spelers[i].Naam(), spelers[i].Positie())
+			switch spelers[i].Positie() {
+			case 63:
+				fmt.Printf("%v heeft gewonnen!\n", spelers[i].Naam())
+				klaar = true
+				break game
+			case 10, 20, 30, 40, 50, 60:
+				fmt.Println("Dubbele stappen!")
+				spelers[i].SetPositie(db.LaatsteWorp())
+				fmt.Printf("Je staat nu op %v\n", spelers[i].Positie())
+			case 23:
+				fmt.Println("Je gaat naar de gevangenis!\nGame over!")
+				klaar = true
+				break game
+			case 25, 45:
+				fmt.Println("Terug naar start!")
+				sp.ToStart()
 			}
 
 		}
